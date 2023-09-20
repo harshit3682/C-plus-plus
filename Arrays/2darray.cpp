611a1db0b86b156f4902c3847cd5cc5ce82bebf3
harshit3682 <harshitrautela3682@gmail.com>
2023-09-20 20:14:53 +0530

    updated

---
+++ Arrays/2darray.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+int printrowsum(int **arr, int row, int col)
+{
+    cout << endl;
+    cout << "Row sum is : ";
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout << sum << " ";
+    }
+}
+
+int printcolsum(int **arr, int row, int col)
+{
+    cout << endl;
+    cout << endl;
+    cout << "Column sum is : ";
+    for (int j = 0; j < col; j++)
+    {
+        int sum1 = 0;
+        for (int i = 0; i < row; i++)
+        {
+            sum1 = sum1 + arr[i][j];
+        }
+        cout << sum1 << " ";
+    }
+}
+
+int printarray(int **arr, int row, int col)
+{
+    cout << "Printing array : " << endl;
+    ;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int m, n, row, col;
+    cout << "Enter row size: ";
+    cin >> m;
+    cout << "Enter column size: ";
+    cin >> n;
+
+    //  int arr[m][n];
+    int **arr = new int *[m];
+    for (int i = 0; i < m; i++)
+    {
+        arr[i] = new int[n];
+    }
+
+    cout << "Input elements :"
+         << " , ";
+    for (row = 0; row < m; row++)
+    {
+        for (col = 0; col < n; col++)
+        {
+            cin >> arr[row][col];
+        }
+    }
+
+    printarray(arr, m, n);
+    printrowsum(arr, m, n);
+    printcolsum(arr, m, n);
+
+    // int arr[3][3];
+    // cout << "Input elements :"
+    //      << " , ";
+    // for (row = 0; row < 3; row++)
+    // {
+    //     for (col = 0; col < 3; col++)
+    //     {
+    //         cin >> arr[row][col];
+    //     }
+    // }
+
+    // printarray(arr, 3, 3);
+    // printrowsum(arr, 3, 3);
+    // printcolsum(arr, 3, 3);
+}
---
+++ OOPs/constructors.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+class hero
+{
+public:
+    char *name;
+    int health;
+    char level;
+
+    // public:
+    hero()
+    {
+        cout << "simple constructor called" << endl;
+        name = new char[100];
+    }
+
+    // parameterized constructor:
+    hero(const char name[], int health, char level)
+    {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+        // this->name = name;
+        this->health = health;
+        this->level = level;
+    }
+
+    hero(int health, char level)
+    {
+        this->health = health;
+        this->level = level;
+    }
+
+    // hero(hero &temp)
+    // {
+    //     cout << endl;
+    //     cout << "Copy constructor called" << endl;
+    //     char *ch = new char[strlen(temp.name) + 1];
+    //     strcpy(ch, temp.name);
+    //     this->name = ch;
+
+    //     this->health = temp.health;
+    //     this->level = temp.level;
+    // }
+    // Both above and below can be used
+    hero(hero &temp)
+    {
+        cout << endl;
+        cout << "Copy constructor called edited version" << endl;
+        this->name = new char[strlen(temp.name) + 1];
+        strcpy(this->name, temp.name);
+        // this->name = ch;
+
+        this->health = temp.health;
+        this->level = temp.level;
+    }
+
+    void setname(const char name[])
+    {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+        // this->name = name;
+    }
+    int sethealth(int health)
+    {
+        this->health = health;
+    }
+    char setlevel(char level)
+    {
+        this->level = level;
+    }
+    char *getname()
+    {
+        return this->name;
+    }
+    int gethealth()
+    {
+        return health;
+    }
+    char getlevel()
+    {
+        return level;
+    }
+    void print()
+    {
+        // cout << endl;
+        cout << "Name: " << this->name << endl;
+        cout << "Health: " << this->health << endl;
+        cout << "Level: " << this->level << endl;
+    }
+    ~hero()
+    {
+        delete[] name;
+        cout << "Destructor is called\n";
+    };
+};
+
+int main()
+{
+    hero paul;
+    // hero paul("paul", 10, 'A');
+    // paul.print();
+
+    // hero *h = new hero("hari", 1, 'C');
+    // h->print();
+
+    // hero sawn("sawn", 22, 'B');
+    // sawn.print();
+    paul.sethealth(12);
+    paul.setlevel('D');
+    char name[7] = "paul";
+
+    paul.setname("fame");
+    cout << endl;
+
+    paul.print();
+
+    // use default copy constructor
+
+    hero eddy(paul);
+    cout << endl;
+    cout << "eddy's data: " << endl;
+    eddy.print();
+    //    Hero hero2 = hero1;
+
+    paul.name[0] = 's';
+    cout << endl;
+    cout << "paul data: " << endl;
+    paul.print();
+    cout << endl;
+    cout << "eddy data: " << endl;
+    eddy.print();
+
+    // hero1 = hero2;
+
+    // hero1.print();
+
+    // hero2.print();
+
+    // eddy.print();
+
+    // paul.name[0] = 'e';
+
+    // default constructor using ptr
+    /*
+     hero *h = new hero;
+
+       h->setname("paul");
+       h->sethealth(20);
+       h->setlevel('C');
+       cout << endl;
+       //.print();
+       cout << "Name: " << (*h).getname() << endl;
+       cout << "Name: " << (h)->getname() << endl;
+       cout << "Health: " << (h)->gethealth() << endl;
+       cout << "Health: " << (*h).gethealth() << endl;
+       cout << "Level: " << (h)->getlevel() << endl;
+       cout << "Level: " << (*h).getlevel() << endl;*/
+
+    // default constructor use
+    /*
+    #include <iostream>
+#include <string.h>
+using namespace std;
+class hero
+{
+    char *name;
+    int health;
+    char level;
+
+public:
+    void setname(const char name[])
+    {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+        // this->name = name;
+    }
+    int sethealth(int health)
+    {
+        this->health = health;
+    }
+    char setlevel(char level)
+    {
+        this->level = level;
+    }
+    char *getname()
+    {
+        return this->name;
+    }
+    int gethealth()
+    {
+        return health;
+    }
+    char getlevel()
+    {
+        return level;
+    }
+};
+    int main()
+    {
+        hero p;
+     p.setname("paul");
+     p.sethealth(10);
+     p.setlevel('A');
+     cout << "Name: " << p.getname() << endl;
+     cout << "Health: " << p.gethealth() << endl;
+     cout << "Level: " << p.getlevel() << endl;
+     }
+     */
+}
